754e2de57bb948b513ecdf904c0671c260c7c21f
larajan67 <50749651+larajan67@users.noreply.github.com>
2019-05-17 10:59:22 -0700

    Update test_r.R

--- test_r.R
+++ test_r.R
@@ -2,4 +2,6 @@ https://github.com/larajan67/reproducibledata_ljansen.git
 #####GIT TEST##
 library(ggplot2)
 lith<-read.csv('~/Desktop/DMgt19/Export_litho.csv')
-library(vegan)
+library(vegan) 
+##test
+head(lith)
